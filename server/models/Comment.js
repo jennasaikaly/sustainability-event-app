3067d4b7c34af01002f8ef403ee2618eaaa776f8
SierraDavis2000 <sierra.davis502@gmail.com>
2022-07-24 00:20:33 -0400

    set up to schema folder

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -8,7 +8,7 @@ const commentSchema  = new Schema({
         minlength: 1,
         maxlength: 280,
     },
-    username: {
+    commentAuthor: {
         type: String,
         required: true,
     },
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,45 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        events: [Event]!
+    }
+    type Event {
+        _id: ID
+        eventTitle: String
+        organizers: String
+        description: String
+        keywords: String
+        location: String
+        eventTiming: String
+        eventFees: String
+        contactInfo: String
+        additionalInfo: String
+        link: String
+        image: String
+        createdAt: String
+        comments: [Comment]!
+    }
+    
+    type Comment {
+        _id: ID
+        commentText: String
+        commentAuthor: String
+        createdAt: String
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        events: [Event]
+    }
+`;
+
 module.exports = typeDefs;
