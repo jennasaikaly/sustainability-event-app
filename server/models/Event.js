8ffc415334bf24e89145670cee0892791376e2ed
SierraDavis2000 <sierra.davis502@gmail.com>
2022-07-23 21:27:23 -0400

    models created

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,2 +1,32 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const eventSchema = new Schema({
+    organizers: [
+        {
+          type: String,
+        },
+    ],
+    eventTitle: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+    
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+})
+    
