c2006a04a93801aca29a92f6e7bf3e2484783817
SierraDavis2000 <sierra.davis502@gmail.com>
2022-07-26 02:05:23 -0400

    updates to middleware functionality

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// TO DO : edit for current project
+
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
@@ -20,6 +20,16 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+ 
+};
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -28,20 +38,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(
+      `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+    );
   });
-};
+});
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
