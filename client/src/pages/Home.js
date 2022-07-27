bf3836c2cbde66fdf1b0a32d18c5c1804d7520be
jennasaikaly <jen.saikaly@gmail.com>
2022-07-27 09:53:17 -0400

    finished login/sign up functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,16 @@ import EventList from '../components/EventList';
 import { useQuery } from '@apollo/client';
 import { QUERY_EVENTS, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+// import savedEvents from '../components/savedEvents';
 
 const Home = () => {
     // use useQuery hook to make query request
     const { loading, data } = useQuery(QUERY_EVENTS);
+
+    //for savedEvents component
+    // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+// const { data: userData } = useQuery(QUERY_ME);
+
    // // if data exists, store it in the events constant we just created. If data is undefined, then save an empty array to the thoughts component.
     const events = data?.events || [];
     // console.log(events);
@@ -14,13 +20,20 @@ const Home = () => {
     return (
         <main>
             <div className="eventlist-container">
-                <div className="col-12 mb-3">
+            <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
                      {loading ? (
             <div>Loading...</div>
         ) : (
             <EventList events={events} title="Some Local Event(s)..." />
         )} 
-                    
+        //the props need to be edited 
+                    {/* {loggedIn && userData ? (
+  <div className="col-12 col-lg-3 mb-3">
+    <SavedEvents
+      savedEvent={userData.me.savedEvent}
+     />
+  </div>
+) : null} */}
                     </div>
             </div>
         </main>
